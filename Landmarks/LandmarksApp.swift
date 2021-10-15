c869008e0df44ad380df13cd14047abdc31bbf46
izumiz <42127166+izumiz-dev@users.noreply.github.com>
2021-10-16 01:27:30 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Izumi Zama on 2021/10/16.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
